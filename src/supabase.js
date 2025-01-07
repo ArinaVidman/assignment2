44baf5b0db304a310e9e6cf6377ff758c523422d
ArinaVidman <arisha.vidman@gmail.com>
2025-01-07 16:41:25 +0000

    Supabase added

--- src/supabase.js
+++ src/supabase.js
@@ -2,3 +2,5 @@ import {createClient} from 'supabase/supabase-js';
 const supabaseUrl = 'https://wafbxiwyfazdanxrtgvn.supabase.co';
 const supabaseKey = process.env.SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+document.addEventListener('DOMContentLoaded', fetchPosts);
